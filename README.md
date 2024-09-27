012453b737ee4631035203e79ab981f97476abae
Doğukan Ürker <dogukanurker@icloud.com>
2024-09-28 02:26:37 +0300

    💀

--- README.md
+++
@@ -1,2 +0,0 @@
-# FlaskBase
-Flask base template
--- routes/index.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-indexBlueprint = Blueprint('index', __name__)
-
-@indexBlueprint.route('/')
-def index():
-    return render_template("index.jinja.html")
